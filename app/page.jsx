d60875a9a379983199dc1ab90e1ec9abf0e4e7bb
shirin-sha <project.manager4565@gmail.com>
2025-08-21 13:17:49 +0530

    metadata changes

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,7 @@
 import Home2 from "./(homes)/home-2/page";
 export const metadata = {
   title: "DAN",
-  description: "Xbuild - Constriction nextjs Template",
+  description: "DAN General Trading & Contracting Co. W.L.L",
 };
 export default function Home() {
   return (
